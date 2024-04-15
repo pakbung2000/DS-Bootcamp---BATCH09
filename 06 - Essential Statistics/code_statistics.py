e56cf4edf49f038ae9066b6256446269072ad98a
Pakbung Pimnara <142192211+pakbung2000@users.noreply.github.com>
2024-04-15 18:02:07 +0700

    Create code_statistics.py

---
+++ 06 - Essential Statistics/code_statistics.py
@@ -0,0 +1,84 @@
+
+import scipy.stats as stats
+import math
+
+### ESTIMATION
+
+# Proportion of one population and two-tailed
+x = 6
+n = 30
+confidence_level = 0.95
+
+p = 6/30
+q = 1-p
+alpha = 1-confidence_level
+se = math.sqrt((p*q)/n)
+
+z = stats.norm.ppf(1-(alpha/2))
+
+higher_bound = round(p + (z*se), 3)
+lower_bound = round(p - (z*se), 3)
+
+print(f"p: {p}")
+print(f"q: {q}")
+print(f"alpha: {alpha}")
+print(f"se: {se}")
+print(f"z: {z}")
+print(f"moe: {z*se}")
+print(f"higher bound: {higher_bound}")
+print(f"lower_bound: {lower_bound}")
+print(f"confidence interval: [{lower_bound}, {higher_bound}]")
+
+
+# Mean of one population and two-tailed
+
+x_bar = 62.1
+s = 13.46
+n = 29
+confidence_level = 0.95
+
+alpha = 1 - confidence_level
+df = n - 1
+t = stats.t.ppf((1-alpha/2), df)
+se = s/math.sqrt(n)
+moe = t*se
+
+lower_bound = x_bar - moe
+upper_bound = x_bar + moe
+
+print(f"1-alpha/2: {1-alpha/2}")
+print(f"t, df = 28: {t}")
+print(f"se: {se}")
+print(f"moe: {moe}")
+print(f"lower_bound: {lower_bound}")
+print(f"upper_bound: {upper_bound}")
+
+### HYPOTHESIS TESTING
+
+# For right-tailed proportion test
+# H0: p <= 0.2
+# Ha: p > 0.2
+
+# Specify the number of occurrences (x), the sample size (n)
+x = 10
+n = 40
+pi = 0.2
+sig_level = 0.05 # Alpha
+# Area H0 = 0.95
+
+p = 10/40
+std_p = math.sqrt((pi*(1-pi))/n)
+
+z_p = ((p-pi)/std_p)
+z_critical = stats.norm.ppf(0.95)
+
+
+
+
+
+
+
+
+
+
+
