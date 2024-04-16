2041d2601ae755aca1917f40f4cd06de0a68bc2a
Pakbung Pimnara <142192211+pakbung2000@users.noreply.github.com>
2024-04-16 15:45:20 +0700

    Update estimation_abtesting.py

--- 06 - Essential Statistics/estimation_abtesting.py
+++ 06 - Essential Statistics/estimation_abtesting.py
@@ -37,7 +37,7 @@ print(f"confidence interval: [{lower_bound}, {higher_bound}]")
 
 x_bar = 62.1
 s = 13.46
-n = 29
+n = 29 # using t-score
 confidence_level = 0.95
 
 alpha = 1 - confidence_level
@@ -58,7 +58,8 @@ print(f"upper_bound: {upper_bound}")
 
 ### HYPOTHESIS TESTING
 
-# 1. Right-tailed proportion test
+# 1. Proportion of one population
+# Right-tailed test
 
 # H0: p <= 0.2
 # Ha: p > 0.2
@@ -69,11 +70,34 @@ pi = 0.2
 sig_level = 0.05 # confident level = 0.95
 
 p = 10/40
-std_p = math.sqrt((pi*(1-pi))/n)
+p_std = math.sqrt((pi*(1-pi))/n)
 
-z_p = ((p-pi)/std_p)
-z_critical = stats.norm.ppf(0.95)
+p_value = ((p-pi)/p_std)
+critical_value = stats.norm.ppf(0.95)
 
+# p_value = 0.7905694150420945
+# critical_value = 1.6448536269514722
+# Since p_value < critical_value, we failed to reject H0
+# We concluded that p <= 0.2
+
+# 2. Mean of one population
+# Two-tailed test
+
+# H0: mu = 200
+# Ha: mu != 200
+
+x_bar = 172.5
+s = 119.5
+mu_null = 200
+n = 40
+alpha = 0.01 # alpha/2 = 0.005
+
+z_score = (x_bar - mu_null)/(s/math.sqrt(n))
+p_value = stats.norm.cdf(z_score)
+
+# p_value = 0.07277352327093886
+# Since p_value > 0.005, we failed to reject H0
+# We concluded that mu = 200
 
 
 
