3b1a591805bb037269aa3ad9a0bd1259472df98f
Pimnara Sujaritchat <142192211+pakbungdesu@users.noreply.github.com>
2024-06-09 08:07:33 +0700

    Update rock-paper-scissor.r

--- 03 - R Programming/rock-paper-scissor.r
+++ 03 - R Programming/rock-paper-scissor.r
@@ -1,14 +1,10 @@
 
-
-# Rock-Paper-Scissor Game
-
 game <- function(){
 
   flush.console()
-  cat("Welcome to Rock - Paper - Scissor Game 🔥\n")
-  cat("WIN +2\nTIED +1\nLOSE 0\n")
-  cat("You must enter [rock], [paper], or [scissor]\n")
+  cat("It's a Rock-Paper-Scissor Game ✂️", "\nWIN +2\nTIED +1\nLOSE 0\n")
 
+  flush.console()
   con <- readline("Do you want to continue or stop?: ")
 
   hand_to_num <- function(hand){
@@ -23,42 +19,48 @@ game <- function(){
 
   score_matrix <- matrix(c(1,2,0,0,1,2,2,0,1), nrow = 3, ncol = 3)
 
-  all_user_score <- 0
-  all_com_score <- 0
+  all_user_score <<- 0
+  all_com_score <<- 0
   i <- 1
 
   while (con != "stop"){
 
   # USER PART
+  flush.console()
   cat("\nRound ", i, "\n")
   flush.console()
-  user_text <- readline("Your hand?: ")
+  user_text <- tolower(as.character(readline("Your hand?: ")))
   user_hand <- hand_to_num(user_text)
 
+  flush.console()
   #COM PART
   all_hands <- c("rock", "paper", "scissor")
   com_text <- sample(all_hands, 1)
   cat("Computer's hand: ", com_text, "\n")
   com_hand <- hand_to_num(com_text)
 
+  flush.console()
   #SCORE
   user_score <- score_matrix[user_hand, com_hand]
   cat("You got ", user_score, "\n")
-  all_user_score <- user_score + all_user_score
+  all_user_score <<- user_score + all_user_score
   com_score <- score_matrix[com_hand, user_hand]
-  all_com_score <- com_score + all_com_score
+  all_com_score <<- com_score + all_com_score
 
+  flush.console()
   # PLAY AGAIN OR NOT
   con <- readline("Do you want to continue or stop?: ")
   i <- i+1
   }
 
+  flush.console()
+  cat("\nYour total score: " , all_user_score, "\nComputer's total score: ", all_com_score)
   if (all_user_score > all_com_score){
-    print("You win! Congratulation ✨")
+    cat("\nYou win! Congratulation ✨")
   }else if (all_user_score < all_com_score){
-    print("You lost. Wanna give another try? 🔥")
+    cat("\nYou lost. Wanna give another try? 🔥")
   }else{
-    print("It's a tied match 🥊")
+    cat("\nIt's a tied match 🥊")
   }
 
 }
@@ -66,6 +68,3 @@ game <- function(){
 
 # CALL FUNCTION
 game()
-
-
-
